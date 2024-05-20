b9c2164f98d982c805915cb781dd3014ef0b05f5
Mizaan-hub <mizaanafandi@gmail.com>
2024-05-20 08:13:39 +0530

    Added toggle/ show-hide feature to FAQ's in script.js, Updated FAQs css and markup

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// NAV BAR SCROLL CHANGE
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
 })
+
+// FAQ's CLASS TOGGLE (FUGA)
+// show/hide answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('fuga');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus";
+        }
+        else{
+            icon.className = "uil uil-plus";
+        }
+    })
+});
